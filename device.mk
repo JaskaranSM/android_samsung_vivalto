bbd0d10a7210be1f2a7629dacc2b4637bfc71ff5
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-07-06 16:58:13 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -11,7 +11,7 @@ DEVICE_PACKAGE_OVERLAYS +=  device/samsung/vivaltods5m/overlay
 
 PRODUCT_COPY_FILES += \
     device/samsung/vivaltods5m/ramdisk/init.hawaii_ss_vivaltods5m.rc:root/init.hawaii_ss_vivaltods5m.rc \
-	device/samsung/bcm-common/ramdisk/fstab.hawaii_ss_vivaltods5mxx:root/fstab.hawaii_ss_vivaltods5m \
+	device/samsung/bcm-common/ramdisk/fstab.hawaii_ss_vivaltods5m:root/fstab.hawaii_ss_vivaltods5m \
     device/samsung/bcm-common/ramdisk/ueventd.hawaii_ss_vivaltods5mxx.rc:root/ueventd.hawaii_ss_vivaltods5m.rc
 
 PRODUCT_PROPERTY_OVERRIDES += \
